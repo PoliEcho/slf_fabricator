912c67f14c7a151143a382c89ff10b104497212a
PoliEcho <poliecho@pupes.org>
2024-06-21 22:33:53 +0200

    args edit

--- fabricator.py
+++ fabricator.py
@@ -10,10 +10,9 @@ parser = argparse.ArgumentParser(
            but it can be changed in Sigma data center program
        '''))
 
-parser.add_argument("-g", "--gpx", dest="gpx", help="gpx file")
-parser.add_argument("-t", "--temperature", dest="temperature", help="temperature during activity")
-parser.add_argument("-s", "--speed", dest="speed", help="speed in km/h cannot be used with --time")
-parser.add_argument("-T", "--time", dest="time", help="length of activity cannot be used with --speed")
+parser.add_argument("-g", "--gpx", dest="gpx", type=argparse.FileType('r', encoding='UTF-8'), required=True, help="gpx file")
+parser.add_argument("-s", "--speed", dest="speed", type=int, help="speed in km/h cannot be used with --time")
+parser.add_argument("-T", "--time", dest="time", type=int, help="length of activity in minutes cannot be used with --speed")
 parser.add_argument("-o", "--output", dest="output", help="output file name")
 
 args = parser.parse_args()
