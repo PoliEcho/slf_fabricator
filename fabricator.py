76094ab221d38e8785cf20e23e2b7e9ee8c7261b
PoliEcho <poliecho@pupes.org>
2024-06-21 15:31:58 +0200

    more args

--- fabricator.py
+++ fabricator.py
@@ -1,11 +1,19 @@
 import xml.etree.ElementTree as ET
 import argparse
+import textwrap
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+    prog='slf_fabricator',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    epilog=textwrap.dedent('''\
+       sport is set to Cycling by default,
+           but it can be changed in Sigma data center program
+       '''))
 
-parser.add_argument("-g", "--gpx",dest ="gpx", help="gpx file")
-parser.add_argument("-t", "--temperature",dest = "temperature", help="temperature during activity")
-parser.add_argument("-c", "--calories",dest = "calories", help="calories burned during activity")
-
+parser.add_argument("-g", "--gpx", dest="gpx", help="gpx file")
+parser.add_argument("-t", "--temperature", dest="temperature", help="temperature during activity")
+parser.add_argument("-s", "--speed", dest="speed", help="speed in km/h cannot be used with --time")
+parser.add_argument("-T", "--time", dest="time", help="length of activity cannot be used with --speed")
+parser.add_argument("-o", "--output", dest="output", help="output file name")
 
 args = parser.parse_args()
